5f9e991437d4e69ab49a43c109702dbec4a5c65b
CodyTseng <cody880528@hotmail.com>
2018-07-02 00:02:51 +0800

    Hello addon

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("divProgress").style.display = "none";
+document.getElementById("divMaskFrame").style.display = "none";
+document.getElementById("page-wrapper").style.visibility = "visible";
+document.getElementById("page-wrapper").style.display = "block";
+document.getElementById("page-footer").style.display = "block";
---
+++ readme.md
@@ -0,0 +1,5 @@
+# FasterE3
+
+https://addons.mozilla.org/zh-TW/firefox/addon/faster-e3/
+
+This addon is created to made [NCTU new e3](https://e3new.nctu.edu.tw/my/) load faster by hiding loading screen immediately after the main page loads, instead of waiting every AJAX request to finish.
