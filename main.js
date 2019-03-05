cc54c6df060e8b763340f05c8d1d431358a6ff61
CodyTseng <cody880528@hotmail.com>
2019-03-06 02:08:26 +0800

    New method

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-document.getElementById("divProgress").style.display = "none";
-document.getElementById("divMaskFrame").style.display = "none";
-document.getElementById("page-wrapper").style.visibility = "visible";
-document.getElementById("page-wrapper").style.display = "block";
-document.getElementById("page-footer").style.display = "block";
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        var el = mutation.target;
+        if (el instanceof HTMLDivElement) {
+            switch (el.id) {
+                case "divProgress":
+                case "divMaskFrame":
+                    el.style.display = "none";
+                    break;
+                case "page-wrapper":
+                    el.style.visibility = "visible";
+                    el.style.display = "block";
+                    break;
+                case "page-footer":
+                    el.style.display = "block";
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+
+observer.observe(document, { childList: true, subtree: true });
+document.addEventListener("DOMContentLoaded", function () { return observer.disconnect(); });
